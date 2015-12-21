6059d3ba35bb5a1e3d167d43b985cfbfe90ed6f8
olitreadwell <olitreadwell@gmail.com>
2015-12-21 13:54:49 -0800

    Fix typo

--- hello/hello.rb
+++ hello/hello.rb
@@ -23,6 +23,6 @@ end
 
 get '/what/time/is/it/in/:number/hours' do
   number = params[:number].to_i
-  time = Time.now + number & 3600
-  "The time in #{number} hours will be #{time.strftime{'%I:%M %p')}"
+  time = Time.now + number * 3600
+  "The time in #{number} hours will be #{time.strftime('%I:%M %p')}"
 end
