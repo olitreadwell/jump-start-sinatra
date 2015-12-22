7bbc3c1fa9a6c12f52067e8d93d742898b2c63df
olitreadwell <olitreadwell@gmail.com>
2015-12-21 20:30:22 -0800

    DRY out with layout template

--- songs-by-sinatra/main.rb
+++ songs-by-sinatra/main.rb
@@ -6,26 +6,29 @@ get '/' do
 end
 
 __END__
-@@home
+@@layout
 <!doctype html>
-  <html lang="en">
-    <head>
-      <title>Songs by Sinatra</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <header>
-        <h1>Songs By Sinatra</h1>
-        <nav>
-          <ul>
-            <li><a href="/" title="Home">Home</a></li>
-            <li><a href="/about" title="About">About</a></li>
-            <li><a href="/contact" title="Contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-      <section>
-        <p>Welcome to this website all about the songs of the great Frank Sinatra</p>
-      </section>
-    </body>
-  </html>
+<html lang="en">
+<head>
+ <title><%= @title %></title>
+ <meta charset="utf-8">
+</head>
+<body>
+ <header>
+   <h1><%= @title %></h1>
+   <nav>
+    <ul>
+      <li><a href="/" title="Home">Home</a></li>
+      <li><a href="/about" title="About">About</a></li>
+      <li><a href="/contact" title="Contact">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+<section>
+  <%= yield %>
+</section>
+</body>
+</html>
+
+@@home
+<p>Welcome to this website all about the songs of the great Frank Sinatra</p>
