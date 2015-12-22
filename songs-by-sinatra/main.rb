47e6b77fa4860b2eaa755fb4297edef893c0dcd8
olitreadwell <olitreadwell@gmail.com>
2015-12-21 20:14:03 -0800

    include inline view for home

--- songs-by-sinatra/main.rb
+++ songs-by-sinatra/main.rb
@@ -9,5 +9,23 @@ __END__
 @@home
 <!doctype html>
   <html lang="en">
-
+    <head>
+      <title>Songs by Sinatra</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <header>
+        <h1>Songs By Sinatra</h1>
+        <nav>
+          <ul>
+            <li><a href="/" title="Home">Home</a></li>
+            <li><a href="/about" title="About">About</a></li>
+            <li><a href="/contact" title="Contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <p>Welcome to this website all about the songs of the great Frank Sinatra</p>
+      </section>
+    </body>
   </html>
