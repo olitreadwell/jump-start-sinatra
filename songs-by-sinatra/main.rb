1790e5890858b25e8c63c2d4147f0ee343a025b9
olitreadwell <olitreadwell@gmail.com>
2015-12-22 18:26:27 -0800

    Move inline views into their own files

--- songs-by-sinatra/main.rb
+++ songs-by-sinatra/main.rb
@@ -13,34 +13,3 @@ end
 get '/contact' do
   erb :contact
 end
-
-__END__
-@@layout
-<!doctype html>
-<html lang="en">
-<head>
- <title><%= @title %></title>
- <meta charset="utf-8">
-</head>
-<body>
- <header>
-   <h1><%= @title %></h1>
-   <nav>
-    <ul>
-      <li><a href="/" title="Home">Home</a></li>
-      <li><a href="/about" title="About">About</a></li>
-      <li><a href="/contact" title="Contact">Contact</a></li>
-    </ul>
-  </nav>
-</header>
-<section>
-  <%= yield %>
-</section>
-</body>
-</html>
-
-@@about
-<p>This site is a demonstration of how to build a website using Sinatra.</p>
-
-@@contact
-<p>You can contact me by sending an email to daz at gmail.com</p>
