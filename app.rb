d73d8247c9a7e77a9132b6736db6e62316fd98fe
olitreadwell <olitreadwell@gmail.com>
2016-01-07 14:08:47 -0600

    Add environment based configurations for a gem & database

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 #REQUIREMENTS
 require 'sinatra'
-# require 'sinatra-contrib'
 require 'sass'
 require 'slim'
 
+configure :development do
+ require 'sinatra/reloader'
+ DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
 
 #ROUTES
 require_relative "routes/main.rb"
